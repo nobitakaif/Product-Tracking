aa712f2b1194193c2ab2e4be081ef549759186dc
mohd kaif <mk2818356@gmail.com>
2025-12-30 03:44:16 +0530

    wrote shipment contract for eth blockchain

--- scripts/send-op-tx.ts
+++ scripts/send-op-tx.ts
@@ -15,7 +15,7 @@ console.log("Sending 1 wei from", senderClient.account.address, "to itself");
 const l1Gas = await publicClient.estimateL1Gas({
   account: senderClient.account.address,
   to: senderClient.account.address,
-  value: 1n,
+  value: BigInt(1),
 });
 
 console.log("Estimated L1 gas:", l1Gas);
@@ -23,7 +23,8 @@ console.log("Estimated L1 gas:", l1Gas);
 console.log("Sending L2 transaction");
 const tx = await senderClient.sendTransaction({
   to: senderClient.account.address,
-  value: 1n,
+  value: BigInt(1),
+
 });
 
 await publicClient.waitForTransactionReceipt({ hash: tx });
