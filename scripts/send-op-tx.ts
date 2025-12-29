797f8a7ba14bb526d172006027b7a61d41335b7a
mohd kaif <mk2818356@gmail.com>
2025-12-30 05:12:33 +0530

    build hardhat contract

--- scripts/send-op-tx.ts
+++ scripts/send-op-tx.ts
@@ -1,32 +1,47 @@
 import { network } from "hardhat";
 
-const { viem } = await network.connect({
-  network: "hardhatOp",
-  chainType: "op",
-});
+const { viem, networkName } = await network.connect({
+  network : "hardhatOp",
+  chainType : "op",
+})
 
-console.log("Sending transaction using the OP chain type");
+async function main(){
+  const Tracking = await viem.deployContract("Tracking")  
+  console.log(`Tracking deployed to ${Tracking.address}`)
+}
 
-const publicClient = await viem.getPublicClient();
-const [senderClient] = await viem.getWalletClients();
+main().catch((e)=>{
+  console.error(e)
+  process.exitCode = 1;
+})
 
-console.log("Sending 1 wei from", senderClient.account.address, "to itself");
+// const { viem } = await network.connect({
+//   network: "hardhatOp",
+//   chainType: "op",
+// });
 
-const l1Gas = await publicClient.estimateL1Gas({
-  account: senderClient.account.address,
-  to: senderClient.account.address,
-  value: BigInt(1),
-});
+// console.log("Sending transaction using the OP chain type");
 
-console.log("Estimated L1 gas:", l1Gas);
+// const publicClient = await viem.getPublicClient();
+// const [senderClient] = await viem.getWalletClients();
 
-console.log("Sending L2 transaction");
-const tx = await senderClient.sendTransaction({
-  to: senderClient.account.address,
-  value: BigInt(1),
+// console.log("Sending 1 wei from", senderClient.account.address, "to itself");
 
-});
+// const l1Gas = await publicClient.estimateL1Gas({
+//   account: senderClient.account.address,
+//   to: senderClient.account.address,
+//   value: BigInt(1),
+// });
 
-await publicClient.waitForTransactionReceipt({ hash: tx });
+// console.log("Estimated L1 gas:", l1Gas);
 
-console.log("Transaction sent successfully");
+// console.log("Sending L2 transaction");
+// const tx = await senderClient.sendTransaction({
+//   to: senderClient.account.address,
+//   value: BigInt(1),
+
+// });
+
+// await publicClient.waitForTransactionReceipt({ hash: tx });
+
+// console.log("Transaction sent successfully");
